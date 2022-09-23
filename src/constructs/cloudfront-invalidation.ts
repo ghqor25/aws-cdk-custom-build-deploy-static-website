0db2b15df39ebd1552d43640d1762d767b210c17
songbaek <ghqor25@naver.com>
2022-09-23 22:38:09 +0900

    fix stepfunction

--- src/constructs/cloudfront-invalidation.ts
+++ src/constructs/cloudfront-invalidation.ts
@@ -8,32 +8,21 @@ interface CloudfrontInvalidationProps {
     * Files in the distribution's edge caches will be invalidated.
     */
    readonly cloudfrontDistributionId: string;
-   /**
-    * interval time for checking whether invalidation has been done.
-    * @default Duration.seconds(30)
-    */
-   readonly queryInterval?: Duration;
-   /**
-    * In some cases, create invalidation might take little longer.
-    * you can manage the timeout for create invalidation handler.
-    * @default Duration.seconds(5)
-    */
-   readonly timeoutCreateInvalidation?: Duration;
-   /**
-    * timeout for total cloudfront invalidation procedure
-    * @default Duration.minutes(10)
-    */
-   readonly timeoutTotal?: Duration;
 }
 
 /**
- * stepFunction stateMachine for cloudfront invalidation
+ * stepFunction stateMachine for cloudfront invalidation.
+ *
+ * It might takes long because create invalidation makes timeout error in some cases,
+ * like doing right after creating cloudfront distribution or validation with no interval.
+ * If stepfunction fails with total 10 minutes timeout, check if there is other problem.
  */
 export class CloudfrontInvalidation extends Construct {
    public readonly stateMachine: aws_stepfunctions.StateMachine;
    constructor(scope: Construct, id: string, props: CloudfrontInvalidationProps) {
       super(scope, id);
-      const cloudfrontCreateInvalidation = new aws_stepfunctions_tasks.LambdaInvoke(this, 'CloudfrontCreateInvalidation', {
+
+      const lambdaCloudfrontCreateInvalidation = new aws_stepfunctions_tasks.LambdaInvoke(this, 'CloudfrontCreateInvalidation', {
          lambdaFunction: new aws_lambda_nodejs.NodejsFunction(this, 'CreateInvalidation', {
             bundling: { minify: true, sourceMap: false, sourcesContent: false, target: 'ES2020' },
             runtime: aws_lambda.Runtime.NODEJS_16_X,
@@ -45,16 +34,19 @@ export class CloudfrontInvalidation extends Construct {
                   resources: [getCloudfrontDistributionArn(Stack.of(this).account, props.cloudfrontDistributionId)],
                }),
             ],
-            timeout: props.timeoutCreateInvalidation ?? Duration.seconds(5),
+            retryAttempts: 0,
          }),
          payload: { type: aws_stepfunctions.InputType.OBJECT, value: { DISTRIBUTION_ID: props.cloudfrontDistributionId } },
+         retryOnServiceExceptions: false,
+         timeout: Duration.minutes(9),
       });
+      lambdaCloudfrontCreateInvalidation.addRetry({ interval: Duration.minutes(1), backoffRate: 1 });
 
       const wait30Secs = new aws_stepfunctions.Wait(this, 'WaitForGetInvalidation', {
-         time: aws_stepfunctions.WaitTime.duration(props.queryInterval ?? Duration.seconds(30)),
+         time: aws_stepfunctions.WaitTime.duration(Duration.seconds(30)),
       });
 
-      const cloudfrontGetInvalidation = new aws_stepfunctions_tasks.LambdaInvoke(this, 'CloudfrontGetInvalidation', {
+      const lambdaCloudfrontGetInvalidation = new aws_stepfunctions_tasks.LambdaInvoke(this, 'CloudfrontGetInvalidation', {
          lambdaFunction: new aws_lambda_nodejs.NodejsFunction(this, 'GetInvalidation', {
             bundling: { minify: true, sourceMap: false, sourcesContent: false, target: 'ES2020' },
             runtime: aws_lambda.Runtime.NODEJS_16_X,
@@ -70,14 +62,14 @@ export class CloudfrontInvalidation extends Construct {
          inputPath: '$.Payload',
       });
 
-      const result = new aws_stepfunctions.Choice(this, 'Invalidation Complete ?', { inputPath: '$.Payload' })
+      const choiceResult = new aws_stepfunctions.Choice(this, 'Invalidation Complete?', { inputPath: '$.Payload' })
          .when(aws_stepfunctions.Condition.stringEquals('$.status', 'SUCCEEDED'), new aws_stepfunctions.Succeed(this, 'Succeeded'))
          .when(aws_stepfunctions.Condition.stringEquals('$.status', 'FAILED'), new aws_stepfunctions.Fail(this, 'Failed'))
          .otherwise(wait30Secs);
 
       this.stateMachine = new aws_stepfunctions.StateMachine(this, 'StateMachine', {
-         definition: cloudfrontCreateInvalidation.next(wait30Secs).next(cloudfrontGetInvalidation).next(result),
-         timeout: props.timeoutTotal ?? Duration.minutes(10),
+         definition: lambdaCloudfrontCreateInvalidation.next(wait30Secs).next(lambdaCloudfrontGetInvalidation).next(choiceResult),
+         timeout: Duration.minutes(10),
       });
    }
 }
--- src/lambda/stepfunctions/cloudfront-invalidation/create-invalidation.ts
+++ src/lambda/stepfunctions/cloudfront-invalidation/create-invalidation.ts
@@ -6,20 +6,16 @@ const handler = async (event: any) => {
    const distributionId = event['DISTRIBUTION_ID'];
    if (!distributionId) throw Error('"DISTRIBUTION_ID" is required ');
 
-   try {
-      const result = await cloudfrontClient.send(
-         new CreateInvalidationCommand({
-            DistributionId: distributionId,
-            InvalidationBatch: { Paths: { Quantity: 1, Items: ['/*'] }, CallerReference: Date.now().toString() },
-         }),
-      );
+   const result = await cloudfrontClient.send(
+      new CreateInvalidationCommand({
+         DistributionId: distributionId,
+         InvalidationBatch: { Paths: { Quantity: 1, Items: ['/*'] }, CallerReference: Date.now().toString() },
+      }),
+   );
 
-      return {
-         INVALIDATION_ID: result.Invalidation?.Id,
-         DISTRIBUTION_ID: distributionId,
-      };
-   } catch (e) {
-      throw Error('createinvalidation error' + e);
-   }
+   return {
+      INVALIDATION_ID: result.Invalidation?.Id,
+      DISTRIBUTION_ID: distributionId,
+   };
 };
 export { handler };
