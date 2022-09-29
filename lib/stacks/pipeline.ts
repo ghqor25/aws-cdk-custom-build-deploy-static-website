fa592394ddbb066c09b3913f1dececca96e37ab1
songbaek <ghqor25@naver.com>
2022-09-29 15:38:53 +0900

    fix sample code

--- lib/stacks/pipeline.ts
+++ lib/stacks/pipeline.ts
@@ -48,7 +48,7 @@ export class PipelineStack extends Stack {
                'dev',
             ),
             installCommands: ['yarn set version 3.2.1', 'yarn install'],
-            commands: ['yarn cdk synth --quiet'],
+            commands: ['yarn synth'],
             primaryOutputDirectory: 'cdk.out',
          }),
          publishAssetsInParallel: true,
